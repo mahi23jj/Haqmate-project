80453a373bd6cbc1d0c6b5eb501ab31acf9a53e7
mahlet <mahlet2ambaye@gmail.com>
2026-01-31 22:59:01 +0000

    testing crocs

--- src/app.ts
+++ src/app.ts
@@ -30,13 +30,20 @@ if (config.isdev) {
   }));
 } else {
   // production CORS settings
+  // app.use(cors({
+  //   origin: [
+  //     "*"
+  //     // "http://localhost:54056/"
+  //   ], // only your deployed frontend
+  //   credentials: true,
+  // }));
   app.use(cors({
-    origin: [
-      // "*"
-      "http://localhost:54056/"
-    ], // only your deployed frontend
+    origin: (origin, callback) => {
+      callback(null, true); // allow all origins
+    },
     credentials: true,
   }));
+
 
 }
 
